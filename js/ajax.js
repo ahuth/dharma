872d49893b562fdf4ee49f9e756e7474dc8bea28
Andrew Huth <andrewhuth2@live.com>
2013-03-19 22:29:07 -0400

    Remived ajax post request

--- js/ajax.js
+++ js/ajax.js
@@ -7,32 +7,6 @@ dharma.ajax = (function (XMLHttpRequest, RSVP) {
     
     var done = 4, ok = 200;
     
-    // Send a post request and return a promise object.  This promise object will
-    // **eventually** hold the response text.
-    function post(url, parameters) {
-    
-        parameters = parameters || '';
-        
-        var XHR = new XMLHttpRequest(),
-            promise = new RSVP.Promise();
-        
-        XHR.open('post', url, true);
-        XHR.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
-        XHR.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        XHR.onreadystatechange = function () {
-            if (XHR.readyState === done) {
-				if (XHR.status === ok) {
-					promise.resolve(XHR.responseText);
-				} else {
-					promise.reject(XHR);
-				}
-            }
-        };
-        XHR.send(parameters);
-        
-        return promise;
-    }
-    
     // Send a get request and return a promise object.  This promise object will
     // **eventually** hold the response text.
     function get(url, parameters) {
@@ -63,7 +37,6 @@ dharma.ajax = (function (XMLHttpRequest, RSVP) {
     
     // API of this module.
     return {
-        post: post,
         get: get
     };
 
