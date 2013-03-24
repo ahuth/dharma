d1508ffd336f5e67c62df040c4f85d0515d36884
Andrew Huth <andrewhuth2@live.com>
2013-03-24 15:51:38 -0400

    Updated comments and CSS

--- js/hotswap.js
+++ js/hotswap.js
@@ -8,12 +8,15 @@ dharma.hotswap = (function (ajax, render) {
 	"use strict";
 	
     // showMain clears the contents of #content, then makes an ajax request for
-    // each widget we want on the page.  I make each request seperately, so that
-    // if one (or more) of them fails, the rest will still load correctly.
+    // each widget we want on the page.  Each request is made seperately, so that
+    // a failure of one won't cause the others to fail.
 	function showMain(level) {
         
 		render.clearInner("content");
 		
+        // Request the karma data.  The first function passed to 'then' is
+        // executed if the request is successful.  The second is executed if the
+        // request fails.
 		ajax.get("php/dharmaservice.php", "type=overview&what=karma&group=jenkintown").then(function (value) {
 			var response = JSON.parse(value);
 			render.renderIntoOrdered("karma", "content", {"value": response.karma.value, "change": response.karma.change});
@@ -21,6 +24,9 @@ dharma.hotswap = (function (ajax, render) {
             render.renderIntoOrdered("fail", "content", {id: "karma", order: "1"});
         });
         
+        // Request the quality data.  The first function passed to 'then' is
+        // executed if the request is successful.  The second is executed if the
+        // request fails.
         ajax.get("php/dharmaservice.php", "type=overview&what=quality&group=jenkintown").then(function (value) {
             var response = JSON.parse(value),
                 turnbacks = response.quality.turnbacks,
@@ -30,12 +36,18 @@ dharma.hotswap = (function (ajax, render) {
             render.renderIntoOrdered("fail", "content", {id: "quality", order: "2"});
         });
         
+        // Request the spending data.  The first function passed to 'then' is
+        // executed if the request is successful.  The second is executed if the
+        // request fails.
         ajax.get("php/dharmaservice.php", "type=overview&what=spending&group=jenkintown").then(function (value) {
             render.renderIntoOrdered("spending", "content", {});
         }, function (value) {
             render.renderIntoOrdered("fail", "content", {id: "spending", order: "3"});
         });
         
+        // Request the production data.  The first function passed to 'then' is
+        // executed if the request is successful.  The second is executed if the
+        // request fails.
         ajax.get("php/dharmaservice.php", "type=overview&what=production&group=jenkintown").then(function (value) {
             render.renderIntoOrdered("production", "content", {});
         }, function (value) {
