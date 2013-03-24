dc6d90907e9b35c298860f6c5d3ea65f7900eb6b
Andrew Huth <andrewhuth2@live.com>
2013-03-23 23:31:17 -0400

    Widget's position = static

--- js/init.js
+++ js/init.js
@@ -4,7 +4,7 @@ var dharma = dharma || {};
 
 dharma.init = (function (hotswap) {
 	"use strict";
-    
+
     hotswap.showMain("jenkintown");
     
 }(dharma.hotswap));
--- php/overview.php
+++ php/overview.php
@@ -5,21 +5,24 @@ namespace overview {
 	function getOverviewData($what, $group) {
 		if ($what == 'karma') {
 			$output['karma'] = getKarmaData($group);
-		}
+		} else if ($what == 'quality') {
+            $output['quality'] = getQualityData($group);
+        }
 		return $output;
 	}
 
 	function getKarmaData($group) {
-        if ($group == 'jenkintown') {
-            $value = '87';
-            $change = '-5%';
-            return array('value'  => $value,
-                         'change' => $change);
-        }
+        $value = '87';
+        $change = '-5%';
+        return array('value'  => $value,
+                     'change' => $change);
 	}
 
 	function getQualityData($group) {
-		
+		$turnbacks = '3';
+        $scrap = 'none';
+        return array('turnbacks' => $turnbacks,
+                     'scrap' => $scrap);
 	}
 
 	function getSpendingData($group) {
