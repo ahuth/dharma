4c94563666e1377fc3020f153492d44d3d45457f
Andrew Huth <andrewhuth2@live.com>
2013-03-29 05:46:34 -0400

    Ajax get function in place

--- js/ajax.js
+++ js/ajax.js
@@ -3,4 +3,45 @@
 
 dharma.ajax = (function (me, rsvp, core) {
 	"use strict";
+    
+    // Send a get request and return a promise object.  This promise object will
+    // **eventually** hold the response text.
+    function get(url, parameters) {
+        
+        var done = 4, ok = 200;
+    
+        if (typeof parameters === "string") {
+            url += "?" + parameters;
+        }
+        
+        var XHR = new XMLHttpRequest(),
+            promise = new rsvp.Promise(),
+            timeout;
+        
+        XHR.open("get", url, true);
+        XHR.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+        XHR.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        XHR.onreadystatechange = function () {
+            if (XHR.readyState === done) {
+                window.clearTimeout(timeout);
+                if (XHR.status === ok) {
+                    promise.resolve(XHR.responseText);
+                } else {
+                    promise.reject(XHR);
+                }
+            }
+        };
+        
+        // If we don't get a response back in 4 seconds, abort the request and
+        // reject the promise object.
+        timeout = setTimeout(function () {
+            XHR.abort();
+            promise.reject(XHR);
+        }, 4000);
+        
+        XHR.send();
+        
+        return promise;
+    }
+    
 }("ajax", parent.RSVP, dharma.core));
--- js/init.js
+++ js/init.js
@@ -3,10 +3,4 @@
 
 dharma.init = (function (me, core) {
 	"use strict";
-	core.subscribe("test", me, function () {
-		alert("Test");
-	});
-	core.publish("test", {});
-    core.unsubscribe("test", me);
-    core.publish("test", {});
 }("init", dharma.core));
