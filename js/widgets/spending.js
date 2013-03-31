b29cc53236c2303e878df9b1806a420802110e6e
Andrew Huth <andrewhuth2@live.com>
2013-03-31 14:18:05 -0400

    spending widget number formatting

--- js/widgets/spending.js
+++ js/widgets/spending.js
@@ -1,7 +1,7 @@
 /*jslint vars: true, browser: true, nomen: true */
 /*global dharma */
 
-dharma.spending = (function (me, document, hogan, ajax, core) {
+dharma.spending = (function (me, document, accounting, hogan, ajax, core) {
     "use strict";
     
     var templates = {
@@ -29,14 +29,25 @@ dharma.spending = (function (me, document, hogan, ajax, core) {
     
     core.subscribe("show-overview", me, function (_group) {
         ajax.get({type: "overview", what: me, group: _group}).then(function (value) {
+            var data;
             if (!value.hasOwnProperty(me)) {
                 renderInto(templates.failure, content, {id: me});
                 return;
             }
-            renderInto(templates.success, content, value[me]);
+            data = {
+                yesterday: accounting.formatMoney(value[me].yesterday, "$", 0),
+                qtd: accounting.formatMoney(value[me].qtd, "$", 0),
+                people: accounting.formatMoney(value[me].people, "$", 0),
+                supplies: accounting.formatMoney(value[me].supplies, "$", 0),
+                tools: accounting.formatMoney(value[me].tools, "$", 0),
+                utilities: accounting.formatMoney(value[me].utilities, "$", 0),
+                maintenance: accounting.formatMoney(value[me].maintenance, "$", 0),
+                other: accounting.formatMoney(value[me].other, "$", 0)
+            };
+            renderInto(templates.success, content, data);
         }, function () {
             renderInto(templates.failure, content, {id: me});
         });
     });
     
-}("spending", parent.document, parent.Hogan, dharma.ajax, dharma.core));
+}("spending", parent.document, parent.accounting, parent.Hogan, dharma.ajax, dharma.core));
