5d7bde1c2fe64213e75c78d68ca82af5b0509ab2
Andrew Huth <andrewhuth2@live.com>
2013-04-04 22:47:21 -0400

    init module comments

--- js/init.js
+++ js/init.js
@@ -4,7 +4,6 @@
 dharma.init = (function (name, core) {
 	"use strict";
     
-	// General control flow subscriptions.
     core.subscribe("widget-clicked", name, function (widget) {
         core.publish("clear-screen");
 		core.publish("show-breakdown", widget);
@@ -24,8 +23,7 @@ dharma.init = (function (name, core) {
     });
 	
 	// Main entry point for the program.  Start off by showing the jenkintown
-    // overview.  Then capture the current state of the page so we can come back
-    // to it later with the back-button.
+    // overview.
     core.publish("show-overview", "jenkintown");
     
 }("init", dharma.core));
