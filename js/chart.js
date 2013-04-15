45ad42bf4ef230658cfe70650a0d3ce75a40998d
Andrew Huth <andrewhuth2@live.com>
2013-04-14 20:13:42 -0400

    Raphy charts...

--- js/chart.js
+++ js/chart.js
@@ -1,15 +1,15 @@
 /*jslint vars: true, browser: true, plusplus: true */
 /*global dharma */
 
-dharma.chart = (function (accounting, Ico) {
+dharma.chart = (function (accounting, Charts) {
 	"use strict";
 	
-	// getDateString converts a date object to a string in the format we need
-	// for our chart.
+	// getDateString converts a date object to a string in the following format:
+	// Apr 14.
 	function getDateString(dateobj) {
-		//var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-		//			  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-		return (dateobj.getMonth() + 1) + "/" + dateobj.getDate();
+		var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+					  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+		return months[dateobj.getMonth()] + " " + dateobj.getDate();
 	}
 	
 	// generateData returns some data in a format that can be directly used by
@@ -23,12 +23,12 @@ dharma.chart = (function (accounting, Ico) {
 		return output;
 	}
 	
-	// generateDates takes some strings, converts them to date objects, and
-	// outputs them back as strings in our desired format.
+	// generateDates takes an array of date strings and converts them to date
+	// objects.
 	function generateDates(dates) {
 		var output = [], item;
 		for (item = 0; item < dates.length; item++) {
-			output.push(getDateString(new Date(dates[item])));
+			output.push(new Date(dates[item]));
 		}
 		return output;
 	}
@@ -44,25 +44,56 @@ dharma.chart = (function (accounting, Ico) {
 		return output;
 	}
 	
+	// makeRaphyData takes the data we've previously generated and puts it in a
+	// data structure that raphy charts understands.
+	function makeRaphyData(dates, values, dots) {
+		if (!dates || !values) {
+			return;
+		}
+		var item, output = [];
+		for (item = 0; item < dates.length; item++) {
+			if (dots) {
+				output.push([dates[item], values[item]]);
+			} else {
+				output.push([dates[item], values[item], {no_dot: true}]);
+			}
+		}
+		return output;
+	}
+	
+	// makeRaphyOptions provides some default raphy charts options, and allows
+	// us to specify our own options.
+	function makeRaphyOptions(userOptions) {
+		var item, options = {
+			show_grid: true,
+			label_max: false,
+			label_min: false,
+			fill_area: true
+		};
+		if (userOptions) {
+			for (item in userOptions) {
+				if (userOptions.hasOwnProperty(item)) {
+					options[item] = userOptions[item];
+				}
+			}
+		}
+		return options;
+	}
+	
 	// drawLineChart draws a line chart which includes our data and areference
 	// line.
 	function drawLineChart(destination, data) {
-		var element = document.getElementById(destination);
-		if (!element) {
-			return;
+		var chartOptions = makeRaphyOptions(),
+			mainLine = makeRaphyData(data.dates, data.data, true),
+			referenceLine = makeRaphyData(data.dates, data.reference, false);
+		
+		var chart = new Charts.LineChart(destination, chartOptions);
+		
+		if (referenceLine) {
+			chart.add_line({data: referenceLine, options: {fill_area: false}});
 		}
-		var chart = new Ico.LineGraph(element, {
-				dataLine: data.data,
-				referenceLine: data.reference
-			}, {
-				markers: "circle",
-				colours: {dataLine: "#00FF00", referenceLine: "#0000FF"},
-				labels: data.dates,
-				meanline: false,
-				grid: true,
-				start_value: 0
-			}
-				);	// This ); is in a weird place because of JSLint.
+		chart.add_line({data: mainLine});
+		chart.draw();
 	}
 	
 	// Module API.
@@ -73,4 +104,4 @@ dharma.chart = (function (accounting, Ico) {
 		drawLineChart: drawLineChart
 	};
 	
-}(parent.accounting, parent.Ico));
+}(parent.accounting, parent.Charts));
