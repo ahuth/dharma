617b2fe67c8362ee9231b4a27d0848135d004c1f
Andrew Huth <andrewhuth2@live.com>
2013-04-20 19:18:54 -0400

    Fixed lowercase

--- js/init.js
+++ js/init.js
@@ -1,19 +1,21 @@
 /*jslint vars: true, browser: true */
 /*global dharma */
 
+// init does some initial setup of the app, manages what happens when a widget or
+// group label get clicked, and starts the ball rolling.
 dharma.init = (function (name, core) {
 	"use strict";
 	
 	core.subscribe("widget-clicked", name, function (group, widget) {
 		core.publish("clear-screen");
-		core.publish("show-breakdown", group, widget);
+		core.publish("show-breakdown", group.toLowerCase(), widget.toLowerCase());
 	});
 	core.subscribe("group-clicked", name, function (group) {
 		core.publish("clear-screen");
-		core.publish("show-overview", group);
+		core.publish("show-overview", group.toLowerCase());
 	});
 	
-	// Main entry point for the program.  Start off by showing the jenkintown
+	// Main entry point for the program.  Start off by showing the Jenkintown
 	// overview.
 	core.publish("show-overview", "jenkintown");
 	
